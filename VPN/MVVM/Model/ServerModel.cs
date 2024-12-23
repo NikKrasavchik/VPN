ea8f3a6dda90a67df5de0b34341f70368cbc40b1
Стачинский Никита <stachinskiy.nikita@gmail.com>
2024-12-23 20:08:13 +0300

    Добавьте файлы проекта.

---
+++ VPN/MVVM/Model/ServerModel.cs
@@ -0,0 +1,11 @@
+﻿namespace VPN.MVVM.ViewModel
+{
+    public class ServerModel
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string server { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ VPN/MVVM/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPN.MVVM.ViewModel
+{
+    class SettingsViewModel
+    {
+    }
+}
